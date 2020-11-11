5102d96d1dd2a914707eec0e8316cd66668dd9d5
Ayoub <m.ayoub@live.nl>
2020-11-11 11:41:49 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+#Readme SteamProject
+##Doel project
+Een grafische weergave van het gaming gedrag van jou vrienden.
+Zoals bijvoorbeeld welke games je vrienden spelen, welke games
+het meest gespeeld worden en wanneer je vrienden online zijn.
+
+##Hoe voer je het programma uit
+1. start het programma "Steam Data Dashboard" op
+2. run dit programma met Python door rechter muis knop te drukken en vervolgens "Open with Python"
+3. Als je dit progamma uitvoerd in een code edditor druk op "Run Code" met als interpreter Python
+4. Hierin zal je alle Data zien
+#### Leden
+Jarno   1781716 jarno.brocker@student.hu.nl  
+Mees    1785659 Mees.soentken@student.hu.nl  
+Max     1783784 max.appeldoorn@student.hu.nl  
+Ayoub   1783513 ayoub.aarkoub@student.hu.nl  
+Gianni  1753114 gianni.zucca@student.hu.nl  
---
+++ Steam Data Dashboard.py
@@ -0,0 +1,33 @@
+from tkinter import *
+import json
+
+with open('steam.json') as jsonFile:
+    data = json.load(jsonFile)
+
+    sortedList = sorted(data, key=lambda x: x['release_date'])
+    EersteItem = data[0]['name']
+
+print(sortedList[-1])
+
+
+root = Tk()
+root.title("Steam Dashboard")
+root.geometry("950x600")
+
+#de  display waarin data wordt weer gegeven
+display = Text(root, background="#f0f0f0", height=18, font=("Helvetica", 16), pady=20)
+display.grid(row=1, column=0)
+display.insert(END, f'''Naam van eerste item in .Json bestand: {EersteItem} \n\n''')
+display.insert(END, f'''Nieuwste game op Steam: {sortedList[-1]['name']}\nRelease: {sortedList[-1]['release_date']}\n\n''')
+display.insert(END, f'''Oudste game op Steam: {sortedList[0]['name']}\nRelease: {sortedList[-1]['release_date']}\n\n''')
+#Dashboard header
+header = Label(master=root,
+              text='Steam Dashboard',
+              background='#171a21',
+              foreground='#969696',
+              font=('Helvetica', 16, 'bold italic'),
+              width=74,
+              height=3)
+header.grid(row=0, column=0)
+
+root.mainloop()
